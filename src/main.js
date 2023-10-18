73bc9df28fbbed66be37843050a3525c8669f466
Shin2k4 <thanhduy2k11995@gmail.com>
2023-10-18 10:01:10 +0700

    projects

---
+++ src/main.js
@@ -0,0 +1,23 @@
+rederHeader();
+
+
+
+let carouselPictures = [
+    "https://layvape.vn/wp-content/uploads/2023/05/Vape-dep-thumbnail.jpg",
+    "assets/img/slideshow_1.png",
+    "https://bizweb.dktcdn.net/thumb/grande/100/247/296/themes/876442/assets/slider_1.jpg?1697522167984"
+]
+renderCarousel(carouselPictures);
+
+let listProducts = [
+    {
+        img: "assets/img/geek-vape-b60.jpg",
+        information: "COMBO DEALS Xlim Se (Tặng Tinh Dầu (30ML), Pod 1 Lần, Dây Đeo, Cáp Sạc)",
+        price: "5000$"
+    },
+    
+]
+listProduct(listProducts)
+
+
+
